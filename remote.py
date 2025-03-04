192480d688560a8c4993923d25153d21c6e98db0
Jay Nipper <jessenipper@gmail.com>
2025-03-03 23:05:01 -0500

    Fix GPIO pull-down constant in IRRemote setup

--- remote.py
+++ remote.py
@@ -5,7 +5,7 @@ class IRRemote:
     def __init__(self, pin):
         self.pin = pin
         GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
-        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUDDOWN)  # Set GPIO pin as input with pull-down resistor
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set GPIO pin as input with pull-down resistor
 
     def read_ir_code(self):
         while True:
