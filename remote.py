e1c9dd9b444b380dfe444649e121cc940c1a8eb4
Jay Nipper <jessenipper@gmail.com>
2025-03-03 22:36:28 -0500

    Refactor lirc initialization in remote control implementation

--- remote.py
+++ remote.py
@@ -2,20 +2,21 @@ import RPi.GPIO as GPIO
 import time
 import lirc
 
-# Initialize the GPIO and lirc
+# Initialize the GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)  # Replace 17 with the GPIO pin you are using
 
-sockid = lirc.init("myremote", blocking=False)
+# Initialize lirc
+lirc_client = lirc.Lirc("myremote", blocking=False)
 
 try:
     while True:
-        code = lirc.nextcode()
+        code = lirc_client.nextcode()
         if code:
             print(f"Received code: {code}")
         time.sleep(0.1)
 except KeyboardInterrupt:
     pass
 finally:
-    lirc.deinit()
+    lirc_client.deinit()
     GPIO.cleanup()
