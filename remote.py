a8c00fefd3ac420dd58b3cfeae3fc1274f42b12b
Jay Nipper <jessenipper@gmail.com>
2025-03-04 00:45:36 -0500

    Integrate pigpio for infrared signal handling and streamline IR code reception

--- remote.py
+++ remote.py
@@ -2,8 +2,10 @@ import RPi.GPIO as GPIO
 import time
 import os
 import sys
+import pigpio
 sys.path.append(os.path.abspath("rpi_ir/config"))
 from load_ir_file import parse_ir_to_dict, find_key
+from rpi_ir.capture.infrared import rx
 
 class IRRemote:
     def __init__(self, pin, ir_code_file):
@@ -12,6 +14,8 @@ class IRRemote:
         GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set GPIO pin as input with pull-down resistor
         self.ir_codes = self.load_ir_codes()
+        self.pi = pigpio.pi()
+        self.ir_receiver = rx(self.pi, self.pin, self.ir_rx_callback, track=False, log=False)
 
     def load_ir_codes(self):
         if os.path.exists(self.ir_code_file):
@@ -22,23 +26,19 @@ class IRRemote:
             print("IR code dictionary file not found.")
             return {}
 
-    def read_ir_code(self):
-        while True:
-            ir_code = self.get_ir_code_from_gpio()
-            if ir_code:
-                key = find_key(self.ir_codes, ir_code)
-                if key:
-                    print(f"Button pressed: {key}")
+    def ir_rx_callback(self, ir_decoded, ir_hex, model, valid, track, log, config_folder):
+        if valid:
+            key = find_key(self.ir_codes, ir_hex)
+            if key:
+                print(f"Button pressed: {key}")
 
-    def get_ir_code_from_gpio(self):
-        # Placeholder function to simulate reading an IR code from GPIO
-        # Replace this with actual implementation to read and decode the IR signal
-        time.sleep(1)  # Simulate waiting for an IR signal
-        return "0xfd00ff"  # Simulate a received IR code
+    def read_ir_code(self):
+        print("Waiting for IR code...")
+        while True:
+            time.sleep(1)  # Keep the script running to receive IR signals
 
 def main():
     ir_remote = IRRemote(pin=17, ir_code_file="rpi_ir/config/ir_code_ff.txt")
-    print("Waiting for IR code...")
     ir_remote.read_ir_code()
 
 if __name__ == "__main__":
