022f565bf8cbb9ddedd62488066f9dd09e8c2b89
Jay Nipper <jessenipper@gmail.com>
2025-03-04 20:43:16 -0500

    Add dotenv support for secret key management and update .gitignore

--- demo1.py
+++ demo1.py
@@ -1,10 +1,16 @@
 from IR.remote import IRRemote
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+secret_key = os.getenv('SECRET_KEY')
 
 def main():
     """
     Main function to create an IRRemote instance and start reading IR codes.
     """
-    ir_remote = IRRemote(pin=17, ir_code_file="./config/ir_code_ff.txt", private_key="UGjIlhTTfcfjwmK6XJWM")  # Replace with your actual private key
+    ir_remote = IRRemote(pin=17, ir_code_file="./config/ir_code_ff.txt", private_key=secret_key)  # Replace with your actual private key
     ir_remote.read_ir_code()
 
 if __name__ == "__main__":
