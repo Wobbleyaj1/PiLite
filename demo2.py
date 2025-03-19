cfd37101b2803fc1defe1c9c2bca8d3b8f01bbf8
Jay Nipper <jessenipper@gmail.com>
2025-03-19 12:54:44 -0400

    Add virtual environment setup and update requirements; create setup instructions

--- demo2.py
+++ demo2.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 from RGB_Strips.rgb_controller import RGBController
 import time
+import signal
+import sys
+from startup import create_and_activate_venv, start_pigpiod, load_environment_variables, cleanup
+
+# Create and activate virtual environment
+venv_path = "/home/pi/PiLite/venv"
+create_and_activate_venv(venv_path)
 
 # Create an instance of RGBController
 controller = RGBController()
